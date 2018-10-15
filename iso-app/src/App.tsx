1d938cddf6b6e5c666384302cf420522d2cf35e3
Richard Knoll <riknoll@microsoft.com>
2018-10-15 15:06:44 -0700

    More work

--- iso-app/src/App.tsx
+++ iso-app/src/App.tsx
@@ -1,22 +1,39 @@
+/// <reference path="../../../pxt/built/pxtsim.d.ts" />
+
+
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    protected embedFrame: HTMLDivElement;
+    protected driver: pxsim.SimulatorDriver;
+
+    private setFrameRef = (div: HTMLDivElement) => this.embedFrame = div;
+
+    public render() {
+        return (
+            <div className="App">
+                <div id="simulator-embed" ref={this.setFrameRef}>
+                </div>
+            </div>
+        );
+    }
+
+    public componentDidMount() {
+        if (this.embedFrame) {
+            this.driver = new pxsim.SimulatorDriver(this.embedFrame, {
+                simUrl: "./simulator.html"
+            });
+              
+            const binRequest = new XMLHttpRequest();
+            const that = this;
+            binRequest.addEventListener("load", function() {
+                that.driver.run(this.responseText);
+            });
+            binRequest.open("GET", "./external/iso-binary.js");
+            binRequest.send();
+        }
+    }
 }
 
 export default App;
